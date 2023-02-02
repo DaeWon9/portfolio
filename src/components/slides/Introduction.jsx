7e6988c69a4f60d4a6a7c7eba3c3e793de0e89b3
DaeWon9 <qkreodnjs97@naver.com>
2023-02-02 15:06:32 +0900

    [ADD] intro text

--- src/components/slides/Introduction.jsx
+++ src/components/slides/Introduction.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TypeAnimation } from 'react-type-animation';
 import "./Slides.css";
 
 const Introduction = () => {
@@ -10,7 +11,23 @@ const Introduction = () => {
             </div>
             <div className="slide_body">
                 <div className="introduction">
-                    안녕하세요 ~~~~개발자 박대원입니다.
+                <TypeAnimation
+                    sequence={[
+                        '안녕하세요',
+                        500,
+                        '안녕하세요 어제보다 오늘, 더 성장하고 싶은 개발자 박대원입니다.',
+                        1000,
+                        '',
+                        () => {
+                        console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                        }
+                    ]}
+                    speed={1}
+                    wrapper="div"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{ fontSize: '1em' }}
+                />
                 </div>
             </div>
         </div>
