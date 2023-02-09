bc813075d9241646b6590b8eec1f44439bcf6686
DaeWon9 <qkreodnjs97@naver.com>
2023-02-09 17:07:33 +0900

    [FIX] mobile skill box scale

--- src/components/skill/SkillBox.jsx
+++ src/components/skill/SkillBox.jsx
@@ -5,17 +5,24 @@ import "./SkillBox.css"
 const SkillBox = (props) => {
 
     return(
-        <motion.div 
-            whileHover={{ scale: 1.1 }} 
-            className="skill_box"
-        >
-            <div className="skill_box_title">
-                {props.title}
+        window.innerWidth >= 480 ? 
+            <motion.div whileHover={{ scale: 1.1 }} className="skill_box">
+                <div className="skill_box_title">
+                    {props.title}
+                </div>
+                <div className="skill_box_icons_container">
+                    {props.icons}
+                </div>
+            </motion.div>
+        :
+            <div className="skill_box">
+                <div className="skill_box_title">
+                    {props.title}
+                </div>
+                <div className="skill_box_icons_container">
+                    {props.icons}
+                </div>
             </div>
-            <div className="skill_box_icons_container">
-                {props.icons}
-            </div>
-        </motion.div>
     )
 }
 
