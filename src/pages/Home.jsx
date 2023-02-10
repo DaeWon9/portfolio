27027e66193afc10a7b2c46967835f53acac4738
DaeWon9 <qkreodnjs97@naver.com>
2023-02-10 17:58:28 +0900

    [ADD] resize event listener

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, Pagination } from 'swiper';
+import { useSetRecoilState } from "recoil";
+import { WINDOW_SIZE } from "../recoil/Atoms";
 import Introduction from "../components/slides/Introduction";
 import Career from "../components/slides/Career";
 import Skills from "../components/slides/Skills";
@@ -14,6 +16,21 @@ import "./Home.css"
 const Home = () => {
     const [swiper, setSwiper] = useState()
     const [isDetailCardOpen, setIsDetailCardOpen] = useState(false)
+    const setWindowSize = useSetRecoilState(WINDOW_SIZE)
+
+    const handleResize= () => {
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        setWindowSize({width: width, height: height})
+    }
+
+    useEffect( () => {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return()=>{
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
 
     useEffect( () => {
         if (swiper){
@@ -31,43 +48,43 @@ const Home = () => {
 
     return(
         <>
-        <NavBar 
-            swiper = {swiper}
-        />
-        <Swiper
-            onSwiper={(swiper) => {
-                setSwiper(swiper)
-            }}
-            direction={"vertical"}
-            slidesPerView={1}
-            mousewheel={true}
-            pagination={{
-                clickable: true,
-            }}
-            allowTouchMove={true}
-            speed={1000}
-            modules={[Mousewheel, Pagination]}
-            className="slider"
-        >
-            <SwiperSlide>
-                {({ isActive }) => <Introduction isActive={isActive}/>}
-            </SwiperSlide>
+            <NavBar 
+                swiper = {swiper}
+            />
+            <Swiper
+                onSwiper={(swiper) => {
+                    setSwiper(swiper)
+                }}
+                direction={"vertical"}
+                slidesPerView={1}
+                mousewheel={true}
+                pagination={{
+                    clickable: true,
+                }}
+                allowTouchMove={true}
+                speed={1000}
+                modules={[Mousewheel, Pagination]}
+                className="slider"
+            >
+                <SwiperSlide>
+                    {({ isActive }) => <Introduction isActive={isActive}/>}
+                </SwiperSlide>
 
-            <SwiperSlide>
-                <Career />
-            </SwiperSlide>
-            
-            <SwiperSlide>
-                {({ isActive }) => <Skills isActive={isActive} swiper={swiper} setIsDetailCardOpen={setIsDetailCardOpen} />}
-            </SwiperSlide>
+                <SwiperSlide>
+                    <Career />
+                </SwiperSlide>
+                
+                <SwiperSlide>
+                    {({ isActive }) => <Skills isActive={isActive} swiper={swiper} setIsDetailCardOpen={setIsDetailCardOpen} />}
+                </SwiperSlide>
 
-            <SwiperSlide>
-                <Project 
-                    isDetailCardOpen = {isDetailCardOpen}
-                    setIsDetailCardOpen = {setIsDetailCardOpen}
-                />
-            </SwiperSlide>
-        </Swiper>
+                <SwiperSlide>
+                    <Project 
+                        isDetailCardOpen = {isDetailCardOpen}
+                        setIsDetailCardOpen = {setIsDetailCardOpen}
+                    />
+                </SwiperSlide>
+            </Swiper>
         </>
     )
 }
