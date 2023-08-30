5124b41e03280c150a6257a629067fa036df809f
DaeWon9 <qkreodnjs97@naver.com>
2023-08-30 17:59:13 +0900

    [FIX] swiper.allowTouchMove

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,68 @@
-import React, { useState, useEffect} from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Mousewheel, Pagination } from 'swiper';
-import { useSetRecoilState } from "recoil";
+import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, Pagination } from "swiper";
+import { useRecoilState } from "recoil";
 import { WINDOW_SIZE } from "../recoil/Atoms";
 import Introduction from "../components/slides/Introduction";
 import Career from "../components/slides/Career";
 import Skills from "../components/slides/Skills";
 import Project from "../components/slides/Project";
 import NavBar from "../components/common/NavBar";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade';
-import "./Home.css"
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import "./Home.css";
 
 const Home = () => {
-    const [swiper, setSwiper] = useState()
-    const [isDetailCardOpen, setIsDetailCardOpen] = useState(false)
-    const setWindowSize = useSetRecoilState(WINDOW_SIZE)
+    const [swiper, setSwiper] = useState();
+    const [isDetailCardOpen, setIsDetailCardOpen] = useState(false);
+    const [windowSize, setWindowSize] = useRecoilState(WINDOW_SIZE);
+    const [isAllowTouchMove, setIsAllowTouchMove] = useState(false);
 
-    const handleResize= () => {
+    const handleResize = () => {
         let width = window.innerWidth;
         let height = window.innerHeight;
-        setWindowSize({width: width, height: height})
-    }
+        setWindowSize({ width: width, height: height });
+    };
 
-    useEffect( () => {
-        window.addEventListener('resize', handleResize);
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
         handleResize();
-        return()=>{
-            window.removeEventListener('resize', handleResize);
-        }
-    }, [])
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
-    useEffect( () => {
-        if (swiper){
-            if (isDetailCardOpen){
-                swiper.mousewheel.disable()
-                swiper.allowTouchMove = false
+    useEffect(() => {
+        if (swiper) {
+            if (isDetailCardOpen) {
+                swiper.mousewheel.disable();
+                swiper.allowTouchMove = false;
+            } else {
+                swiper.mousewheel.enable();
+                swiper.allowTouchMove = true;
             }
-            else{
-                swiper.mousewheel.enable()
-                swiper.allowTouchMove = true
+        }
+    }, [isDetailCardOpen]);
+
+    useEffect(() => {
+        if (swiper) {
+            if (windowSize.width > 480) {
+                swiper.allowTouchMove = false;
+            } else {
+                swiper.allowTouchMove = true;
             }
         }
+    }, [windowSize]);
 
-    }, [isDetailCardOpen])
+    console.log(windowSize.width > 480 ? false : true);
 
-    return(
+    return (
         <>
-            <NavBar 
-                swiper = {swiper}
-            />
+            <NavBar swiper={swiper} />
             <Swiper
                 onSwiper={(swiper) => {
-                    setSwiper(swiper)
+                    setSwiper(swiper);
                 }}
                 direction={"vertical"}
                 slidesPerView={1}
@@ -61,32 +70,29 @@ const Home = () => {
                 pagination={{
                     clickable: true,
                 }}
-                allowTouchMove={true}
+                allowTouchMove={false}
                 speed={1000}
                 modules={[Mousewheel, Pagination]}
                 className="slider"
             >
-                <SwiperSlide>
-                    {({ isActive }) => <Introduction isActive={isActive}/>}
-                </SwiperSlide>
+                <SwiperSlide>{({ isActive }) => <Introduction isActive={isActive} />}</SwiperSlide>
 
                 <SwiperSlide>
                     <Career />
                 </SwiperSlide>
-                
+
                 <SwiperSlide>
-                    {({ isActive }) => <Skills isActive={isActive} swiper={swiper} setIsDetailCardOpen={setIsDetailCardOpen} />}
+                    {({ isActive }) => (
+                        <Skills isActive={isActive} swiper={swiper} setIsDetailCardOpen={setIsDetailCardOpen} />
+                    )}
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <Project 
-                        isDetailCardOpen = {isDetailCardOpen}
-                        setIsDetailCardOpen = {setIsDetailCardOpen}
-                    />
+                    <Project isDetailCardOpen={isDetailCardOpen} setIsDetailCardOpen={setIsDetailCardOpen} />
                 </SwiperSlide>
             </Swiper>
         </>
-    )
-}
+    );
+};
 
 export default Home;
