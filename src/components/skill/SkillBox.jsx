2351de36e1f70ecc186508c45dd0860462dfa2a4
DaeWon9 <qkreodnjs97@naver.com>
2024-03-15 19:07:49 +0900

    [FIX] skillBox

--- src/components/skill/SkillBox.jsx
+++ src/components/skill/SkillBox.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { useRecoilValue } from "recoil";
 import { WINDOW_SIZE } from "../../recoil/Atoms";
-import { motion } from "framer-motion"
-import "./SkillBox.css"
+import "./SkillBox.css";
 
 const SkillBox = (props) => {
-    
-    const windowSize = useRecoilValue(WINDOW_SIZE)
+    const [isHover, setIsHover] = useState(false);
+    const [isShowExtra, setIsShowExtra] = useState(false);
+    const windowSize = useRecoilValue(WINDOW_SIZE);
 
-    return(
-        windowSize.width >= 480 ? 
-            <motion.div whileHover={{ scale: 1.1 }} className="skill_box">
-                <div className="skill_box_title">
-                    {props.title}
-                </div>
-                <div className="skill_box_icons_container">
-                    {props.icons}
-                </div>
-            </motion.div>
-        :
-            <div className="skill_box">
-                <div className="skill_box_title">
-                    {props.title}
-                </div>
-                <div className="skill_box_icons_container">
-                    {props.icons}
-                </div>
+    useEffect(() => {
+        if (windowSize.width > 480 && isHover) {
+            setIsShowExtra(true);
+        } else {
+            setIsShowExtra(false);
+        }
+    }, [isHover]);
+
+    return (
+        <motion.div
+            style={{ border: `0.2rem solid ${props.color}` }}
+            whileHover={{ scale: 1.1 }}
+            className="skill_box"
+            onHoverStart={() => {
+                setIsHover(true);
+            }}
+            onHoverEnd={() => {
+                setIsHover(false);
+            }}
+        >
+            <div
+                className="skill_name"
+                style={{ color: "#060919", backgroundColor: props.color, border: `0.2rem solid ${props.color}` }}
+            >
+                {props.name}
             </div>
-    )
-}
+            {isShowExtra || windowSize.width < 480 ? (
+                <div className="skill_contents">
+                    {props.contents.map((content, idx) => (
+                        <div key={idx}>{content}</div>
+                    ))}
+                </div>
+            ) : (
+                <div className="skill_chip_contents">{props.chips}</div>
+            )}
+        </motion.div>
+    );
+};
 
 export default SkillBox;
